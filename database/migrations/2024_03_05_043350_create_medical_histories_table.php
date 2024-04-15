a077f0446432af0ed2b0114b8a82a0ae243e45a5
shohbozuz <shohbozbek.uz24@gmail.com>
2024-04-15 10:27:32 +0500

    bot to'girlandi

--- database/migrations/2024_03_05_043350_create_medical_histories_table.php
+++ database/migrations/2024_03_05_043350_create_medical_histories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('history');
             $table->foreignId('patient_id')->constrained()->cascadeOnDelete();
+
         });
     }
 
